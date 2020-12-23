ea7c32e4b3eb1460925946dc992f451318822d2d
KAZUKISHUTO <kat0yu0704@gmail.com>
2020-12-23 22:47:10 +0900

    js修正

--- js/main.js
+++ js/main.js
@@ -32,7 +32,31 @@ $(() => {
   })
 });
 
-$(function () {
+
+
+// ======= header =========
+// スクロールしたらヘッダー固定
+let header = $('.header');
+let navLink = $('.header_nav_link');
+let logoW = $('.header_logo-w');
+let logoB = $('.header_logo-b');
+
+$(window).scroll(function () {
+  
+  if ($(window).scrollTop() > 80) {
+    header.addClass('fix');
+    navLink.addClass('fix');
+    logoW.addClass('fix');
+    logoB.addClass('fix');
+  } else {
+    header.removeClass('fix');
+    navLink.removeClass('fix');
+    logoW.removeClass('fix');
+    logoB.removeClass('fix');
+  }
+});
+
+
 
   // ①タブをクリックしたら発動
   $('.tab li').click(function () {
@@ -53,50 +77,26 @@ $(function () {
 
 
 
-// ======= header =========
-// スクロールしたらヘッダー固定
-let header = $('.header');
-let navLink = $('.header_nav_link');
-let logoW = $('.header_logo-w');
-let logoB = $('.header_logo-b');
-
-$(window).scroll(function () {
-
-    if ($(window).scrollTop() > 80) {
-    header.addClass('fix');
-    navLink.addClass('fix');
-    logoW.addClass('fix');
-    logoB.addClass('fix');
-    } else {
-    header.removeClass('fix');
-    navLink.removeClass('fix');
-    logoW.removeClass('fix');
-    logoB.removeClass('fix');
-    }
-});
 
 
+  AOS.init({
+    // Global settings:
+    disable: false, //AOSを無効にする場合に設定。「 true」 で無効。「 'phone'」「'tablet'」「'mobile'」でそれぞれの時に無効。
+    // startEvent: 'DOMContentLoaded', //AOSが初期化されるイベント名を設定。
+    initClassName: 'aos-init', //初期化後に適用されるクラス名
+    animatedClassName: 'aos-animate', //アニメーションに適用されるクラス
+    useClassNames: false, //trueの場合、 スクロール時に「 data - aos」 のクラス名を追加します
+    disableMutationObserver: false, //自動変異の検出を無効にします
+    debounceDelay: 50, //ウィンドウのサイズ変更中に使用されるデバウンスの遅延
+    throttleDelay: 99, //ページのスクロール中に使用されるスロットルの遅延
 
-
-
-AOS.init({
-  // Global settings:
-  disable: false, //AOSを無効にする場合に設定。「 true」 で無効。「 'phone'」「'tablet'」「'mobile'」でそれぞれの時に無効。
-  // startEvent: 'DOMContentLoaded', //AOSが初期化されるイベント名を設定。
-  initClassName: 'aos-init', //初期化後に適用されるクラス名
-  animatedClassName: 'aos-animate', //アニメーションに適用されるクラス
-  useClassNames: false, //trueの場合、 スクロール時に「 data - aos」 のクラス名を追加します
-  disableMutationObserver: false, //自動変異の検出を無効にします
-  debounceDelay: 50, //ウィンドウのサイズ変更中に使用されるデバウンスの遅延
-  throttleDelay: 99, //ページのスクロール中に使用されるスロットルの遅延
-
-  // Effect settings:
-  offset: 50, //元のトリガーポイントからのオフセット（ ピクセル単位）
-  delay: 200,  //アニメーションが始まるまでの遅延時間（0-3000の間で指定。50ms毎）
-  duration: 1500, //アニメーション開始から終了までの時間（0-3000の間で指定。50ms毎）
-  easing: 'ease-out', //AOSアニメーションのイージング
-  once: true, //スクロール中にアニメーションを１回だけ実行するか
-  mirror: false, //スクロール中にアニメーションするか
-  anchorPlacement: 'center-bottom', //トリガーポイントの設定。要素のどの位置がウィンドウのどの位置にきたら実行させるか
-  // （'要素位置-ウィンドウ位置'）位置は「 top」「 center」「 bottom」 でそれぞれ指定。
-});
+    // Effect settings:
+    offset: 50, //元のトリガーポイントからのオフセット（ ピクセル単位）
+    delay: 200,  //アニメーションが始まるまでの遅延時間（0-3000の間で指定。50ms毎）
+    duration: 1500, //アニメーション開始から終了までの時間（0-3000の間で指定。50ms毎）
+    easing: 'ease-out', //AOSアニメーションのイージング
+    once: true, //スクロール中にアニメーションを１回だけ実行するか
+    mirror: false, //スクロール中にアニメーションするか
+    anchorPlacement: 'center-bottom', //トリガーポイントの設定。要素のどの位置がウィンドウのどの位置にきたら実行させるか
+    // （'要素位置-ウィンドウ位置'）位置は「 top」「 center」「 bottom」 でそれぞれ指定。
+  });
